c0e3f47c942920dadb7d5223c229fd9a017bd499
kierepka1972-design <kierepka1972@gmail.com>
2026-02-02 00:05:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-import os
-import time
-import requests
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-def wyslij_wiadomosc(tekst):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": tekst
-    }
-    r = requests.post(url, json=payload)
-    print(r.text)
-
 if __name__ == "__main__":
-    while True:
-        wyslij_wiadomosc("✅ TEST OK – bot działa (heartbeat)")
-        time.sleep(30)
+    wyslij_wiadomosc("✅ TEST OK – BOT DZIAŁA")
